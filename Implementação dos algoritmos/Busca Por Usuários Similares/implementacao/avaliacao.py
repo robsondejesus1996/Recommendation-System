3b1220b3e2c1c142031e489223f804f842447ec4
Robson <robsondejesus1996@hotmail.com>
2021-07-23 14:34:07 -0300

    Inicialização do projeto e teste do Dataset

---
+++ Implementação dos algoritmos/Busca Por Usuários Similares/implementacao/avaliacao.py
@@ -0,0 +1,12 @@
+from recomendacao import avaliacoes
+
+#apenas pegando a avaliação da Ana para o filme Star Trek
+print(avaliacoes['Ana']['Star Trek'])
+
+#pegando a nota que o Marcos deu para o filme Star trek
+print(avaliacoes['Marcos']['Star Trek'])
+
+#buscar todas as avaliações que a ana fez
+print(avaliacoes['Ana'])
+
+
---
+++ Implementação dos algoritmos/Busca Por Usuários Similares/implementacao/recomendacao.py
@@ -0,0 +1,52 @@
+avaliacoes = {'Luiz':
+                  {'Freddy x Jason': 2.5,
+                   'O Ultimato Bourne': 3.5,
+                   'Star Trek': 3.0,
+                   'Exterminador do Futuro': 3.5,
+                   'Norbit': 2.5,
+                   'Star Wars': 3.0},
+
+              'Caio':
+                  {'Freddy x Jason': 3.0,
+                   'O Ultimato Bourne': 3.5,
+                   'Star Trek': 1.5,
+                   'Exterminador do Futuro': 5.0,
+                   'Star Wars': 3.0,
+                   'Norbit': 3.5},
+
+              'Brenda':
+                  {'Freddy x Jason': 2.5,
+                   'O Ultimato Bourne': 3.0,
+                   'Exterminador do Futuro': 3.5,
+                   'Star Wars': 4.0},
+
+              'Aline':
+                  {'O Ultimato Bourne': 3.5,
+                   'Star Trek': 3.0,
+                   'Star Wars': 4.5,
+                   'Exterminador do Futuro': 4.0,
+                   'Norbit': 2.5},
+
+              'Jean':
+                  {'Freddy x Jason': 3.0,
+                   'O Ultimato Bourne': 4.0,
+                   'Star Trek': 2.0,
+                   'Exterminador do Futuro': 3.0,
+                   'Star Wars': 3.0,
+                   'Norbit': 2.0},
+
+              'Yasmin':
+                  {'Freddy x Jason': 3.0,
+                   'O Ultimato Bourne': 4.0,
+                   'Star Wars': 3.0,
+                   'Exterminador do Futuro': 5.0,
+                   'Norbit': 3.5},
+
+              'Robson':
+                  {'O Ultimato Bourne': 4.5,
+                   'Norbit': 1.0,
+                   'Exterminador do Futuro': 4.0}
+              }
+
+
+#estrutura do arquivo são vários objetos como lista alinhadas, um arquivo json
